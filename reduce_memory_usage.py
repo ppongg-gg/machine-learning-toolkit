9f8cc1fd2a8c21cdc5c9abe82c909ab51bf34d19
ppongg-gg <138104404+ppongg-gg@users.noreply.github.com>
2024-11-05 16:08:46 +0700

    Create reduce_memory_usage.py

---
+++ reduce_memory_usage.py
@@ -0,0 +1,34 @@
+def reduce_memory_usage(df):
+    start_mem = df.memory_usage().sum() / 1024**2
+    print(f"Memory usage of dataframe is {start_mem:.2f} MB")
+
+    for col in df.columns:
+        col_type = df[col].dtype
+        
+        # Optimize numerical columns
+        if col_type != object and not pd.api.types.is_categorical_dtype(df[col]):
+            c_min, c_max = df[col].min(), df[col].max()
+            if str(col_type).startswith('int'):
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                else:
+                    df[col] = df[col].astype(np.int64)
+            elif str(col_type).startswith('float'):
+                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                    df[col] = df[col].astype(np.float16)
+                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                    df[col] = df[col].astype(np.float32)
+                else:
+                    df[col] = df[col].astype(np.float64)
+        else:
+            df[col] = df[col].astype('category')
+
+    end_mem = df.memory_usage().sum() / 1024**2
+    print(f"{col} column memory usage after optimization is {end_mem:.2f} MB")
+    print(f"Decreased by {(100 * (start_mem - end_mem) / start_mem):.1f}%\n")
+    gc.collect()
+    return df
